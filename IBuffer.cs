2ff210e34aa7f5c43f5d8e75ad7c6bcb113b0027
wangyimin <wangyimin@yahoo.com>
2024-10-27 15:05:59 +0900

    lock(exclusive access)

--- IBuffer.cs
+++ IBuffer.cs
@@ -15,20 +15,33 @@ namespace DummyMailServer
         private readonly byte[] buff = new byte[1024];
         private int pos = 0;
 
-        public int Length => pos;
+        public int Length
+        {
+            get { lock (buff) { return pos; } }
+        }
 
         public byte[] Get()
         {
-            var r = new byte[pos];
-            Array.Copy(buff, r, pos);
-            return r;
-        }
-        public void Append(byte[] buffer, int start, int length)
-        {
-            Array.Copy(buffer, start, buff, pos, length);
-            pos += length;
+            lock (buff)
+            {
+                var r = new byte[pos];
+                Array.Copy(buff, r, pos);
+                return r;
+            }
         }
 
-        public void Reset() => pos = 0;
+        public void Append(byte[] buffer, int start, int length)
+        {
+            lock (buff)
+            {
+                Array.Copy(buffer, start, buff, pos, length);
+                pos += length;
+            }
+        }
+
+        public void Reset()
+        {
+            lock (buff) { pos = 0; };
+        }
     }
 }
